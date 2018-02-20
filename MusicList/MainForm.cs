f67bfbf9435b25d296e619a93233cf1489530e0f
thanhdung96 <14dh110224@st.huflit.edu.vn>
2018-02-20 21:35:33 +0700

    init project

---
+++ MusicList/MainForm.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using MaterialSkin.Controls;
+using MaterialSkin;
+
+namespace MusicList
+{
+	public partial class MainForm : MaterialForm
+	{
+		public MainForm()
+		{
+			InitializeComponent();
+			
+			InitTheme();
+		}
+		
+		private void InitTheme(){
+			MaterialSkinManager manager = MaterialSkinManager.Instance;
+			manager.AddFormToManage(this);
+			manager.Theme = MaterialSkinManager.Themes.DARK;
+			manager.ColorScheme = new ColorScheme(Primary.BlueGrey800, Primary.BlueGrey900, Primary.BlueGrey500, Accent.LightBlue200, TextShade.WHITE);
+		}
+	}
+}
