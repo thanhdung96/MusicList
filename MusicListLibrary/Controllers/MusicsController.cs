a3831b385f84e2b629cf8b241013210483a5988f
thanhdung96 <14dh110224@st.huflit.edu.vn>
2018-04-25 11:47:17 +0700

    moved MusicsController.cs to appropriate folder

---
+++ MusicListLibrary/Controllers/MusicsController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+using MusicListLibrary.Models;
+using MusicListLibrary.Repositories;
+
+namespace MusicListLibrary.Controllers
+{
+	public sealed class MusicsController
+	{
+		private readonly MusicsRepository repo;
+		
+		public MusicsController()
+		{
+			repo = new MusicsRepository();
+		}
+		
+		public bool IsExist(Musics music)
+		{
+			return repo.FindOne(music) != null;
+		}
+		
+		public void AddMusic(ref Musics music){
+			repo.AddOne(ref music);
+		}
+		
+		public void UpdateOne(ref Musics music){
+			repo.UpdateOne(ref music);
+		}
+	}
+}
